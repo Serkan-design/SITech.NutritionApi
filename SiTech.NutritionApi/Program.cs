929540eed7a3a7b419077a5e997281c9eea9a1eb
Serkan-design <serkanisik67@gmail.com>
2026-03-24 23:16:52 +0300

    aSD

--- SiTech.NutritionApi/Program.cs
+++ SiTech.NutritionApi/Program.cs
@@ -1,12 +1,6 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
-if (builder.Environment.IsDevelopment())
-{
-    builder.Services.AddSwaggerGen();
-}
 
 var app = builder.Build();
 
@@ -14,15 +8,7 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// ❌ swagger sadece development
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.RoutePrefix = "swagger";
-    });
-}
+// ❌ SWAGGER YOK ARTIK
 
 //app.UseHttpsRedirection();
 app.UseAuthorization();
